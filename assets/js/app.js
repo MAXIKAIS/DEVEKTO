770423ba55ba110c044fe96db9cd162040a789d1
Max <maximek4iser@gmail.com>
2020-11-26 19:14:46 +0100

    maj avant deploiement

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,7 +17,7 @@ console.log('Hello Webpack Encore! Edit me in assets/app.js');
 
 // fonction pour afficher mon nom
 var i=0;
-var mytext="Web & Mobile Developer";
+var mytext="Developpeur Web & Mobile";
 var mytextelem=document.getElementById("subtitle");
 var mycurrenttext="";
 function myFunction () {
@@ -33,7 +33,7 @@ function myFunction () {
 myFunction ();
 
 $(document).ready(function(){
-    $('#portfolio .thumbnail').hover(function(){
+    $('#interests .thumbnail').hover(function(){
             $(this).children(".cust-caption").slideDown();
         },
         function(){
